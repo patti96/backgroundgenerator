659e90cd91ef257afa8f72ef0056b6a540f7f170
Patricia <patriciahoffmann1996@web.de>
2020-03-19 16:36:32 +0100

    adding starting project

---
+++ script.js
@@ -0,0 +1,45 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button")
+
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+
+
+
+function bgColors() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    return bgColor;
+    }
+
+function setRandomColors() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ bgColors()
+	+ ", " 
+	+ bgColors()
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+button.addEventListener("click", setRandomColors);
